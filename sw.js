60b7db1064bfa3271662de9069dc99f36866f051
Yakov Bronnikov <52982731+yakovbronnikov@users.noreply.github.com>
2024-09-06 08:41:29 +1000

    feat: cache only

--- sw.js
+++ sw.js
@@ -5,3 +5,28 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
     console.log('Активирован');
 });
+
+const CACHE = 'cache-only-v1';
+
+// При установке воркера мы должны закешировать часть данных (статику).
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE).then((cache) => {
+            return cache.addAll([
+                '/img/background'
+            ]);
+        })
+    );
+});
+
+// При запросе на сервер (событие fetch), используем только данные из кэша.
+self.addEventListener('fetch', (event) =>
+    event.respondWith(fromCache(event.request));
+);
+
+function fromCache(request) {
+    return caches.open(CACHE).then((cache) =>
+      cache.match(request)
+          .then((matching) => matching || Promise.reject('no-match'))
+    );
+}
