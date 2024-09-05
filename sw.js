36eb05e814bd742f23569ad3b702bdd74844e4c6
Yakov Bronnikov <52982731+yakovbronnikov@users.noreply.github.com>
2024-09-06 09:31:36 +1000

    fix: sw settings

--- sw.js
+++ sw.js
@@ -6,29 +6,71 @@ self.addEventListener('activate', (event) => {
     console.log('Активирован');
 });
 
-const CACHE = 'cache-only-v1';
+
+const CACHE = 'offline-fallback-v1';
 
 // При установке воркера мы должны закешировать часть данных (статику).
 self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE).then((cache) => {
-            return cache.addAll([
+        caches
+            .open(CACHE)
+            .then((cache) => cache.addAll([
                 'assets',
                 'deposit-calculator',
+                'credit-calculator',
+                'payback-period',
+                'index.html',
+                'style.css',
+                'script.js',
+                'main.css',
                 'services'
-            ]);
-        })
+            ]))
+            // `skipWaiting()` необходим, потому что мы хотим активировать SW
+            // и контролировать его сразу, а не после перезагрузки.
+            .then(() => self.skipWaiting())
     );
 });
 
-// При запросе на сервер (событие fetch), используем только данные из кэша.
-self.addEventListener('fetch', (event) =>
-    event.respondWith(fromCache(event.request))
-)
+self.addEventListener('activate', (event) => {
+    // `self.clients.claim()` позволяет SW начать перехватывать запросы с самого начала,
+    // это работает вместе с `skipWaiting()`, позволяя использовать `fallback` с самых первых запросов.
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+    // Можете использовать любую стратегию описанную выше.
+    // Если она не отработает корректно, то используейте `Embedded fallback`.
+
+    event.respondWith(fromCache(event.request));
+    event.waitUntil(update(event.request));
+    
+    event.respondWith(networkOrCache(event.request)
+        .catch(() => useFallback()));
+});
+
+function networkOrCache(request) {
+    return fetch(request)
+        .then((response) => response.ok ? response : fromCache(request))
+        .catch(() => fromCache(request));
+}
+
+// Наш Fallback вместе с нашим собсвенным Динозавриком.
+const FALLBACK =
+    '<div>\n' +
+    '    <div>You are offline</div>\n' +
+    '    <img src="assets/offline.svg" alt="offline"/>\n' +
+    '</div>';
+
+// Он никогда не упадет, т.к мы всегда отдаем заранее подготовленные данные.
+function useFallback() {
+    return Promise.resolve(new Response(FALLBACK, { headers: {
+        'Content-Type': 'text/html; charset=utf-8'
+    }}));
+}
 
 function fromCache(request) {
     return caches.open(CACHE).then((cache) =>
-      cache.match(request)
-          .then((matching) => matching || Promise.reject('no-match'))
-    );
+        cache.match(request).then((matching) =>
+            matching || Promise.reject('no-match')
+        ));
 }
