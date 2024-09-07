e44ec37b0dc1d8f83252aa9a2bddc931f2702ece
Yakov Bronnikov <52982731+yakovbronnikov@users.noreply.github.com>
2024-09-07 10:58:54 +1000

    fix: sw link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     // Весь код регистрации у нас асинхронный.
-    navigator.serviceWorker.register('./sw.js')
+    navigator.serviceWorker.register('https://github.com/yakovbronnikov/ainom/blob/main/sw.js')
       .then(() => navigator.serviceWorker.ready.then((worker) => {
         worker.sync.register('syncdata');
       }))
