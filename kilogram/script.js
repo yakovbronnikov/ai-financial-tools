a2f897356c9975ef41dfb0e679f371c83a44e8c9
Yakov Bronnikov <52982731+yakovbronnikov@users.noreply.github.com>
2024-09-20 03:04:54 +1000

    feat: product list

--- kilogram/script.js
+++ kilogram/script.js
@@ -4,9 +4,19 @@ let weight = document.getElementById('weight')
 let weightType = document.getElementById('weight-type')
 let calculateButton = document.getElementById('calculate')
 
+let addButton = document.getElementById('add-product')
+
 let resultPrice = document.getElementById('result-price')
 
+let productList = document.getElementById('product-list')
+let productName = document.getElementById('product-name')
+let productListStorage = []
 
+
+if(localStorage.getItem('tasks')) {
+  productListStorage = JSON.parse(localStorage.getItem('tasks'))
+  addProduct()
+}
 
 function calc(weightType) {
     priceKilo = 0
@@ -17,11 +27,8 @@ function calc(weightType) {
     priceKilo = Number(price.value) / Number(weight.value)
   }
 
-  // resultPrice.innerHTML = "$ " + priceKilo.toFixed(2) + " / kg"
-
-
   resultPrice.innerHTML = ""
-  resultPrice.innerHTML = `<span>$</span>${priceKilo.toFixed(2)}<span>/ kg</span>`
+  resultPrice.innerHTML = `$ ${priceKilo.toFixed(2)}`
 
   skeleton()
 }
@@ -40,12 +47,64 @@ function skeleton() {
 
   resultPrice.classList.add("skeleton-appearance")
   document.querySelector(".price-skeleton").classList.add("result-skeleton")
-
+  
 
   setTimeout(() => {
     resultPrice.classList.remove("skeleton-appearance")
     document.querySelector(".price-skeleton").classList.remove("result-skeleton")
   }, "1200");
+}
+
+
+
+
+
+
+
+addButton.addEventListener('click',function(){
+  let priceKg = Number(price.value) / Number(weight.value) * 1000 
+  priceKg = priceKg.toFixed(2)
+  let product = productName.value == "" ? "Product" : productName.value
+  
+  let newProduct = {
+    name: product,
+    price: priceKg,
+    weight: weight.value
+  }
+
+
+  productListStorage.push(newProduct)
+  addProduct()
+  calc(weightType.value)
+  localStorage.setItem('tasks', JSON.stringify(productListStorage))
+  productName.value = ''
+})
+
+
+function addProduct() {
+  let productLayout = ''
+  
+  productListStorage.forEach(function(item, i) {
+    productLayout += 
+    `
+    <div class="product-item">
+      <div class="product-info">
+        <p class="product-name">${item.name}</p>
+        <p>${item.weight}${weightType.value == "Grams" ? "g" : "kg"}</p>
+        <p>$ ${item.price}/kg</p>
+      </div>
+      <button class="delete-product" onclick="deleteProduct(event, ${i})"></button>
+    </div>
+    `
+  })
+
+  productList.innerHTML = productLayout
+}
+
+function deleteProduct(event, id) {
+  productListStorage.splice(id, 1)
+  localStorage.setItem('tasks', JSON.stringify(productListStorage))
+  addProduct()
 }
 
 
