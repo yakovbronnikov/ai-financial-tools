80869cc48d3b8cdf05edfada8e0fa5430f25e160
Yakov Bronnikov <52982731+yakovbronnikov@users.noreply.github.com>
2024-10-07 15:18:32 +0500

    fix: install safari

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+let install = document.querySelector('#install .heading')
+let safari = navigator.userAgent.toLowerCase().includes('safari')
+let ios = navigator.userAgent.toLowerCase().includes('ios')
+
+if (safari && ios) {
+  install.innerHTML = `
+  <h2>Install the AINOM app</h2>
+  <ul>
+      <li>Tap the share button, it's located at the bottom center</li>
+      <li>Select "Add to home screen" item</li>
+      <li>Come up with a name for the shortcut and tap the add button</li>
+      <li>Wait until the installation is complete</li>
+      <li>Done, enjoy!</li>
+  </ul>
+  `
+} else {
+  install.innerHTML = `
+  <h2>Install the AINOM app</h2>
+  <ul>
+      <li>Without leaving this page, open the browser menu (usually three dots in the upper right corner)</li>
+      <li>Find the item “Add to home screen” or “Install”</li>
+      <li>Confirm the action if necessary</li>
+      <li>Wait until the installation is complete</li>
+      <li>Done, enjoy!</li>
+  </ul>
+  `
+}
+
+let userAgent = navigator.userAgent.toLowerCase();
+
+
+
 if ('serviceWorker' in navigator) {
     // Весь код регистрации у нас асинхронный.
     navigator.serviceWorker.register('https://github.com/yakovbronnikov/ainom/blob/main/sw.js')
@@ -6,3 +38,4 @@ if ('serviceWorker' in navigator) {
       }))
       .catch((err) => console.log(err));
 }
+
